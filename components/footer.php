fb5f2655ad142668a8a0be88128e39caeed39fcb
dodinovembri <dodinovembri32gmail.com>
2022-10-25 14:40:41 +0700

    update

--- components/footer.php
+++ components/footer.php
@@ -3,8 +3,54 @@
     <div class="footer-top clearfix">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-center about-col mb-4">
                     <p>Jalan Melati No 21C, Kebon Jeruk, Jakarta Barat <br> Buka Jam: 05.00 Sore - 11.00 Malam</p>
+                    <p class="mb-0 mb-md-3">Phone: <a href="tel:+6282183599197">0821-8359-9197</a></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer-links">
+                    <h4 class="h4">Policy</h4>
+                    <ul>
+                        <li><a href="my-account.html">Privacy Policy</a></li>
+                        <li><a href="#">Terms &amp; condition</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-12 col-md-4 col-lg-2 footer-links">
+                    <h4 class="h4">Informations</h4>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Location</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer-links">
+                    <h4 class="h4">Customer Services</h4>
+                    <ul>
+                        <li><a href="faqs-style1.html">FAQ's</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-4 newsletter-col">
+                    <div class="display-table pt-md-3 pt-lg-0">
+                        <div class="display-table-cell footer-newsletter">
+                            <form action="#" method="post">
+                                <label class="h4">NEWSLETTER SIGN UP</label>
+                                <p>Enter Your Email To Receive Daily News And Get 20% Off Coupon For All Items.</p>
+                                <div class="input-group">
+                                    <input type="email" class="brounded-start input-group__field newsletter-input mb-0" name="EMAIL" value="" placeholder="Email address" required>
+                                    <span class="input-group__btn">
+                                        <button type="submit" class="btn newsletter__submit rounded-end" name="commit" id="Subscribe"><i class="an an-envelope-l"></i></button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <ul class="list-inline social-icons mt-3 pt-1">
+                        <li class="list-inline-item"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook"><i class="an an-facebook" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter"><i class="an an-twitter" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram"><i class="an an-instagram" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="TikTok"><i class="an an-tiktok" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Whatsapp"><i class="an an-whatsapp" aria-hidden="true"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -12,7 +58,7 @@
     <div class="footer-bottom clearfix">
         <div class="container">
             <div class="d-flex-center flex-column justify-content-md-between flex-md-row-reverse">
-                <div class="copytext text">All Rights Reserved.</div>
+                <div class="copytext text">&copy; 2022 Nadi Service. All Rights Reserved.</div>
             </div>
         </div>
     </div>
