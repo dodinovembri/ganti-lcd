6d6bbca0b43b7db3159d1a5db0d991b79ad921b0
dodinovembri <dodinovembri32gmail.com>
2022-10-27 10:11:44 +0700

    add product

---
+++ terms.php
@@ -0,0 +1,28 @@
+<?php include('components/heading.php') ?>
+
+<!--Body Container-->
+<div id="page-content">
+    <?php include('components/home.php') ?>
+    <!-- List Product -->
+    <div class="container">
+        <div class="row">
+            <!--Blog Post-->
+            <section class="section home-blog-post">
+                <div class="container">
+                    <div class="section-header">
+                        <h2>Lokasi Ganti LCD Hp - Nadi Service</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.536792911367!2d106.76154811430995!3d-6.192673962393819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f71bec3fd2d9%3A0xf7ae320e9d457a91!2sJl.%20Melati%20No.21C%2C%20RT.13%2FRW.10%2C%20Kb.%20Jeruk%2C%20Kec.%20Kb.%20Jeruk%2C%20Kota%20Jakarta%20Barat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2011530!5e0!3m2!1sen!2sid!4v1666757424105!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--End Blog Post-->
+        </div>
+    </div>
+</div>
+<!--End Body Container-->
+
+<?php include('components/footing.php') ?>
